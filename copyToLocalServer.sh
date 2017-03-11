49948b2ac809fc720fbdaf7c95f746fe2a4e51d3
bullbullboy <gtrewqasdf@gmail.com>
2017-03-11 14:20:08 +0900

    ブルの当たり判定&赤色フラッシュを実装

--- copyToLocalServer.sh
+++ copyToLocalServer.sh
@@ -2,8 +2,7 @@
 
 cp ./index.html /Applications/MAMP/htdocs/bullbullboy.github.io
 cp -r ./assets /Applications/MAMP/htdocs/bullbullboy.github.io
+
 #cd ../
-
 #cp -r ./bullbullboy.github.io /Applications/MAMP/htdocs/bullbullboy.github.io
-
 #cd ./bullbullboy.github.io
