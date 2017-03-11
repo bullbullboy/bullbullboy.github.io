055fcf70275503b3f480507c535c02556e53f9bb
bullbullboy <gtrewqasdf@gmail.com>
2017-03-11 11:23:27 +0900

    ダーツボード設置

---
+++ copyToLocalServer.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp ./index.html /Applications/MAMP/htdocs/bullbullboy.github.io
+#cd ../
+
+#cp -r ./bullbullboy.github.io /Applications/MAMP/htdocs/bullbullboy.github.io
+
+#cd ./bullbullboy.github.io
