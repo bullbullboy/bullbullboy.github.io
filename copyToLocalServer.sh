a150898d332c484be79afe9354d215a0c6481649
bullbullboy <gtrewqasdf@gmail.com>
2017-03-11 11:40:32 +0900

    ブル追加

--- copyToLocalServer.sh
+++ copyToLocalServer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp ./index.html /Applications/MAMP/htdocs/bullbullboy.github.io
+cp -r ./assets /Applications/MAMP/htdocs/bullbullboy.github.io
 #cd ../
 
 #cp -r ./bullbullboy.github.io /Applications/MAMP/htdocs/bullbullboy.github.io
