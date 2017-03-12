9eb42a8a994f86132601b48731db745d2a1e86d5
bullbullboy <gtrewqasdf@gmail.com>
2017-03-12 20:07:23 +0900

    [fix]カッコぬけ修正

--- js/main.js
+++ js/main.js
@@ -538,9 +538,7 @@
         this.twitterButton = game.add.image(this.backGroundImg.x, this.backGroundImg.y + SHARE_BUTTON_OFFSET_Y, 'twitterLogo');
         this.twitterButton.scale.setTo(0.15, 0.15);
         this.twitterButton.inputEnabled = true;
-        this.twitterButton.events.onInputUp.add(() => {
-            tweetScore();
-        });
+        this.twitterButton.events.onInputUp.add(tweetScore);
 
         this.resultMenuComponentList.push(this.twitterButton);
 
